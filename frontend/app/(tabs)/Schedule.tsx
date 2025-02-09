98c37a3eabe8311c811a0a22763fa26aaf81175d
Isha Bhandari <ibhandar@vols.utk.edu>
2025-02-09 13:50:36 -0500

    Bare bones schedule page created- just has title and help button

---
+++ frontend/app/(tabs)/Schedule.tsx
@@ -0,0 +1,51 @@
+// Very bare bones- need to have their full schedule in a scrollable view
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+export default function VitalsPage() {
+  const handleButtonPress = (buttonName) => {
+    alert(`Button "${buttonName}" pressed`);
+  };
+  
+  return (
+    // Title needs to be moved up to the top of the page
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.helpButton}
+        onPress={() => handleButtonPress("Help Button")}
+      >
+        <Text style={styles.helpButtonText}>?</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.text}>Schedule</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  helpButton: {
+    position: 'absolute', 
+    top: 40, 
+    right: 20, 
+    width: 60, 
+    height: 60, 
+    backgroundColor: '#FF0000', // Red
+    borderRadius: 30, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+  },
+  helpButtonText: {
+    color: '#fff', 
+    fontSize: 30, 
+    fontWeight: 'bold',
+  }
+});

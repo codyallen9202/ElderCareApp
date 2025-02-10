47e526d0dbba6c0a43a2e4a25556d141d1c044dc
codyallen9202 <codyman9202@gmail.com>
2025-02-10 13:37:25 -0500

    edited navbar and deleted a few default pages that we won't need

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,9 +1,15 @@
+// This is the default entry point for the entire tab bar
+// Equivalent to App.tsx from older versions of React Native
+// Should only be used as a container to call navigation
+
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,26 +18,51 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        headerShown: true,
       }}>
       <Tabs.Screen
-        name="index"
+        name="HomePage"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Medications"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          title: 'Meds',
+          tabBarIcon: ({ color }) => <FontAwesome5 size={28} name="pills" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="Vitals"
+        options={{
+          title: 'Vitals',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heartbeat" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="CheckMedications"
+        options={{
+          title: 'CheckMed',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heartbeat" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Schedule"
+        options={{
+          title: 'Schedule',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="CaregiversList"
+        options={{
+          title: 'Caregivers',
+          tabBarIcon: ({ color }) => <FontAwesome5 size={28} name="user-friends" color={color} />,
+        }}
+      />
+
     </Tabs>
   );
 }

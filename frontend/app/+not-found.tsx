d5557c2ace423280dd1f29a6da383cf246b8e496
codyallen9202 <codyman9202@gmail.com>
2025-02-13 15:25:27 -0500

    deleted default components and fixed 'not found' page to not use the deleted components

--- frontend/app/+not-found.tsx
+++ frontend/app/+not-found.tsx
@@ -1,19 +1,23 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+// Catch-all screen for nonexistent pages
+import { Link, Stack, useRouter } from 'expo-router';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function NotFoundScreen() {
+  const router = useRouter();
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/(tabs)/HomePage" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <Stack.Screen options={{ title: 'Oops! Page not found' }} />
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={[styles.button, styles.button]}
+          onPress={() => router.push('/')}
+        >
+          <FontAwesome size={64} name="home" color={'#fff'} />
+          <Text style={styles.buttonText}>Go Home</Text>
+        </TouchableOpacity>
+      </View>
     </>
   );
 }
@@ -23,10 +27,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    padding: 0,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D1001F',
+    width: '40%',
+    height: '15%',
+    borderRadius: 24
   },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
 });

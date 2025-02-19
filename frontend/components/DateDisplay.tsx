00bc5c715be3d66c2fe1d0e89e5ca4f938b5e9d1
codyallen9202 <codyman9202@gmail.com>
2025-02-18 19:14:25 -0500

    added component that has the current date for use on different pages

---
+++ frontend/components/DateDisplay.tsx
@@ -0,0 +1,33 @@
+// Displays the date in format of
+//    Weekday
+// Day Month Year
+// Used on Meds page and possibly calendar
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import getDate from '@/functions/get-date';
+
+export default function DateDisplay() {
+    const today = getDate();
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.weekday}>{today[3]}</Text>
+            <Text style={styles.date}>{today[2]} {today[1]} {today[0]}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    weekday: {
+        fontWeight: 400,
+        fontSize: 36,
+    },
+    date: {
+        fontWeight: 300,
+        fontSize: 25,
+    }
+});

7b5d07a1bb28c205b258c25fbe9597a49c0ab24d
codyallen9202 <codyman9202@gmail.com>
2025-02-28 11:02:12 -0500

    defined types for explicit declaration

--- frontend/components/DisplayEvents.tsx
+++ frontend/components/DisplayEvents.tsx
@@ -2,25 +2,43 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
 
 // Sharing data between components
-const EventsContext = createContext([]);
+const EventsContext = createContext<EventsContextType | undefined>(undefined);
 
 export const getEvents = () => {
-  return useContext(EventsContext);
+  const context = useContext(EventsContext);
+  if (!context) {
+    throw new Error("getEvents must be used within an EventsProvider");
+  }
+  return context;
 };
 
 // This was added because I kept getting an error when just trying to use childen belo2
 // Learned about this at https://stackoverflow.com/questions/75695037/how-to-use-an-interface-with-props-children-and-other-property-types
 interface EventsProviderProps {
-  children: ReactNode; 
+  children: ReactNode;
+}
+
+// Explicit definition of an event
+interface EventType {
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+}
+
+// Giving context for what the function will call
+interface EventsContextType {
+  events: EventType[];
+  setEvents: React.Dispatch<React.SetStateAction<EventType[]>>;
 }
 
 // Am currently using dummy data, but will soon get data from the FireBase
-export const EventsProvider: React.FC<EventsProviderProps>  = ({ children }) => {
-  const [events, setEvents] = useState([
+export const EventsProvider: React.FC<EventsProviderProps> = ({ children }) => {
+  const [events, setEvents] = useState<EventType[]>([
     {
       title: 'Lunch',
       description: 'Sandwiches today',
-      date: '2025-02-26', 
+      date: '2025-02-26',
       time: '12:00 PM',
     },
     {
@@ -29,7 +47,7 @@ export const EventsProvider: React.FC<EventsProviderProps>  = ({ children }) => 
       date: '2025-02-26',
       time: '2:00 PM',
     },
-  ]);  
+  ]);
 
   return (
     <EventsContext.Provider value={{ events, setEvents }}>

28da7dc1d69a24f598e77faebcb9b3db701d49f0
RobKing2003 <robert.w.king2003@gmail.com>
2025-03-25 14:49:49 -0400

    Update DRAFT_database.js

--- backend/DRAFT_database.js
+++ backend/DRAFT_database.js
@@ -11,17 +11,17 @@ export const setupDatabse = () => {
     // User Table
     db.transaction(tx => {
         tx.executeSql(
-            'CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);'
+            'CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT );'
         );
 
         // Medications Table
         tx.executeSql(
-            'CREATE TABLE IF NOT EXISTS medications ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);'
+            'CREATE TABLE IF NOT EXISTS medications ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT );'
         );
 
         // Caregivers Table
         tx.executeSql(
-            'CREATE TABLE IF NOT EXISTS caregivers ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);'
+            'CREATE TABLE IF NOT EXISTS caregivers ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT );'
         );
     });
 };

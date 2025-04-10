4148f2286ba1f24406e25ea78e25949fb75ae509
codyallen9202 <codyman9202@gmail.com>
2025-04-10 14:11:21 -0400

    added file to hold functions

--- frontend/functions/add-medicaton.tsx
+++
@@ -1,5 +0,0 @@
-// Function that will add a medication to the backend storage
-
-export default function addMedication() {
-
-}
---
+++ frontend/functions/handle-medication.tsx
@@ -0,0 +1,10 @@
+import { doc, setDoc, getDoc, collection, deleteDoc } from "firebase/firestore";
+import { db } from "../firebaseconfig";
+
+export async function addMedication() {
+
+}
+
+export async function deleteMedication() {
+
+}

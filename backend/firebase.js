4296be8121ed33232debb7091f29dc677166e27c
codyallen9202 <codyman9202@gmail.com>
2025-04-11 09:41:40 -0400

    put firebase.js keys in env variables

--- backend/firebase.js
+++ backend/firebase.js
@@ -5,18 +5,18 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB25IT2JY6Avzzgnu2Pi9VKiqFgaGj4Q5Q",
-  authDomain: "eldercareapp-7e8af.firebaseapp.com",
-  projectId: "eldercareapp-7e8af",
-  storageBucket: "eldercareapp-7e8af.firebasestorage.app",
-  messagingSenderId: "100066008222",
-  appId: "1:100066008222:web:863a7bb86c042e6ddb6f8f",
-  measurementId: "G-CK9HKJBT00"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 
-if (!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 
 }
 export { firebase };

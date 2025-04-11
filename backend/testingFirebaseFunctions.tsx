11789b7ebde85113dd21e769a0eef42ac762899e
Cody Allen <113146358+codyallen9202@users.noreply.github.com>
2025-04-11 15:27:37 -0400

    Delete extra backend directory

--- backend/testingFirebaseFunctions.tsx
+++
@@ -1,5 +0,0 @@
-import { addNewDocument, fetchCollectionData } from "./firebasefunctions";
-const name = "TestCollection";
-console.log(fetchCollectionData(name));
-const newStuff = {name: "Bob", age: 7, location: "Knoxville"};
-addNewDocument(name, newStuff);
